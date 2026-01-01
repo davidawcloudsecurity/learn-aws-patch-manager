9dca0d71c94f89b0f514782ea27a4306208407f7
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-01-01 21:26:23 +0800

    Update windows-instances.tf - fix: Fixed both instances to use data.aws_subnets.existing_public[0].ids[0]

--- infra_terraform/windows-instances.tf
+++ infra_terraform/windows-instances.tf
@@ -110,7 +110,7 @@ resource "aws_instance" "wsus_server_2019" {
   count                       = var.create_windows_instances ? 1 : 0
   ami                         = data.aws_ami.windows_2019.id
   instance_type              = "t3.medium"
-  subnet_id                  = can(aws_subnet.public_subnet_01[0].id) ? aws_subnet.public_subnet_01[0].id : data.aws_subnets.existing_public.ids[0]
+  subnet_id                  = can(aws_subnet.public_subnet_01[0].id) ? aws_subnet.public_subnet_01[0].id : data.aws_subnets.existing_public[0].ids[0]
   vpc_security_group_ids     = can(data.aws_security_group.existing_windows_sg.id) ? [data.aws_security_group.existing_windows_sg.id] : [aws_security_group.windows_sg[0].id]
   associate_public_ip_address = true
   iam_instance_profile       = can(data.aws_iam_instance_profile.existing_ssm_profile.arn) ? data.aws_iam_instance_profile.existing_ssm_profile.name : aws_iam_instance_profile.ssm_profile[0].name
@@ -186,7 +186,7 @@ resource "aws_instance" "windows_client_2016" {
   count                       = var.create_windows_instances ? 1 : 0
   ami                        = "ami-0d8940f0876d45867" # "ami-02f5c360d1593d538" windows 2016
   instance_type              = "t3.small"
-  subnet_id                  = can(aws_subnet.public_subnet_01[0].id) ? aws_subnet.public_subnet_01[0].id : data.aws_subnets.existing_public.ids[0]
+  subnet_id                  = can(aws_subnet.public_subnet_01[0].id) ? aws_subnet.public_subnet_01[0].id : data.aws_subnets.existing_public[0].ids[0]
   vpc_security_group_ids     = can(data.aws_security_group.existing_windows_sg.id) ? [data.aws_security_group.existing_windows_sg.id] : [aws_security_group.windows_sg[0].id]
   associate_public_ip_address = true
   iam_instance_profile       = can(data.aws_iam_instance_profile.existing_ssm_profile.arn) ? data.aws_iam_instance_profile.existing_ssm_profile.name : aws_iam_instance_profile.ssm_profile[0].name
