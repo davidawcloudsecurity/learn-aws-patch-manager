1292c2eb2d6456372a7f081c3f45fc7952b81b8e
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-01-22 20:40:13 +0800

    Conditional outputs based on variable flags

--- infra_terraform/outputs.tf
+++ infra_terraform/outputs.tf
@@ -1,27 +1,27 @@
 output "aws_vpc_main" {
-  value = aws_vpc.demo_main_vpc[0].id
+  value = var.create_vpc ? aws_vpc.demo_main_vpc[0].id : null
 }
 
 output "aws_subnet_public" {
-  value = aws_subnet.public_subnet_01[0].id
+  value = var.create_vpc ? aws_subnet.public_subnet_01[0].id : null
 }
 
 output "aws_subnet_private" {
-  value = aws_subnet.private_subnet_01[0].id
+  value = var.create_vpc ? aws_subnet.private_subnet_01[0].id : null
 }
 
 output "wsus_server_public_ip" {
-  value = aws_instance.wsus_server_2019.public_ip
+  value = var.enable_windows_instances ? aws_instance.wsus_server_2019[0].public_ip : null
 }
 
 output "wsus_server_private_ip" {
-  value = aws_instance.wsus_server_2019.private_ip
+  value = var.enable_windows_instances ? aws_instance.wsus_server_2019[0].private_ip : null
 }
 
 output "windows_client_public_ip" {
-  value = aws_instance.windows_client_2016.public_ip
+  value = var.enable_windows_instances ? aws_instance.windows_client_2016[0].public_ip : null
 }
 
 output "windows_client_private_ip" {
-  value = aws_instance.windows_client_2016.private_ip
+  value = var.enable_windows_instances ? aws_instance.windows_client_2016[0].private_ip : null
 }
