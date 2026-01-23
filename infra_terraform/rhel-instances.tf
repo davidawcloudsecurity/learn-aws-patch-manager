ff8a80ecd258c4bf3ffc9fadbb11a718d2d304d4
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2026-01-23 13:32:07 +0800

    Modify user data to comment out dnf update
    Comment out the dnf update command in user data.

--- infra_terraform/rhel-instances.tf
+++ infra_terraform/rhel-instances.tf
@@ -77,8 +77,8 @@ resource "aws_instance" "rhel_9_server" {
   
   user_data = <<-EOF
     #!/bin/bash
-    dnf update -y
     dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    # dnf update -y   
     systemctl enable amazon-ssm-agent
     systemctl start amazon-ssm-agent
     EOF
