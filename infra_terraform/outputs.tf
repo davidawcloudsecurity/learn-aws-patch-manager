c4158196130bddfcb4eadfa80e423dec0960969a
David <davidaw@davidawcloudsecurity.com>
2026-03-15 14:21:20 +0800

    fix: remove duplicates and enable one windows instance

--- infra_terraform/outputs.tf
+++ infra_terraform/outputs.tf
@@ -31,17 +31,9 @@ output "tgw_outbound_id" {
 }
 
 output "wsus_server_public_ip" {
-  value = var.enable_windows_instances ? aws_instance.wsus_server_2019[0].public_ip : null
+  value = var.create_windows_instances ? aws_instance.wsus_server_2019[0].public_ip : null
 }
 
 output "wsus_server_private_ip" {
-  value = var.enable_windows_instances ? aws_instance.wsus_server_2019[0].private_ip : null
-}
-
-output "windows_client_public_ip" {
-  value = var.enable_windows_instances ? aws_instance.windows_client_2016[0].public_ip : null
-}
-
-output "windows_client_private_ip" {
-  value = var.enable_windows_instances ? aws_instance.windows_client_2016[0].private_ip : null
+  value = var.create_windows_instances ? aws_instance.wsus_server_2019[0].private_ip : null
 }
